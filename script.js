0d6222c654e60adfe7d57f65c7bca154ab08d86d
Muaz <muazislambabar@gmail.com>
2026-07-11 15:36:42 +0500

    const variable

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-            // Creating my first variable
+// Creating my first variable
 
-let variableOne
-console.log(variableOne)
-let name = "Sally"
-console.log(name)
+let variableOne;
+console.log(variableOne);
+let name = "Sally";
+console.log(name);
 
-let age = "25"
-console.log(age)
+let age = "25";
+console.log(age);
 
+// Updating variable Values
 
-            // Updating variable Values
+name = "Muaz";
+console.log(name);
 
-name = "Muaz"
-console.log(name)
+let score = 200;
+console.log(score);
 
-let score = 200
-console.log(score)
+score = 20;
+console.log(score);
 
-score= 20
-console.log(score)
+score = 30;
+console.log(score);
 
-score = 30
-console.log(score)
+// Creating const
 
-            // Creating const
+const MY_NAME = "Basit Ali";
 
-const MY_NAME = "Basit Ali"
+// MY_NAME = "Muaz"; // ❌ TypeError: Assignment to constant variable
 
-MY_NAME = "Muaz"    // ❌ TypeError: Assignment to constant variable
+const TEXT =
+  "Const cannot be declared without a value. It needs a default value";
 
-const TEXT = "Const cannot be declared without a value. It needs a default value"
-
-const message   // ❌ SyntaxError: Missing initializer in const declaration
+const message =  // ❌ SyntaxError: Missing initializer in const declaration
