9806bea4b75513196638441e84d70e4f7e8afe44
Muaz <muazislambabar@gmail.com>
2026-07-11 16:29:38 +0500

    Creating funstion

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-// Creating my first variable
+            // Creating function in js 
 
-let variableOne;
-console.log(variableOne);
-let name = "Sally";
-console.log(name);
+// function sayHi() {
+//     console.log("Hi")
+// }
 
-let age = "25";
-console.log(age);
-
-// Updating variable Values
-
-name = "Muaz";
-console.log(name);
-
-let score = 200;
-console.log(score);
-
-score = 20;
-console.log(score);
-
-score = 30;
-console.log(score);
-
-// Creating const
-
-const MY_NAME = "Basit Ali";
-
-// MY_NAME = "Muaz"; // ❌ TypeError: Assignment to constant variable
-
-const TEXT =
-  "Const cannot be declared without a value. It needs a default value";
-
-const message =  // ❌ SyntaxError: Missing initializer in const declaration
+// sayHi()
+// sayHi()
+// sayHi()
