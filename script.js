0369d9d52c7c269a03572a81822964ba329054ac
Muaz <muazislambabar@gmail.com>
2026-07-11 16:40:07 +0500

    Returning values from function

--- script.js
+++ script.js
@@ -8,22 +8,34 @@
 // sayHi()
 // sayHi()
 
-function printName(name) {
-    console.log(name)
+// function printName(name) {
+//     console.log(name)
+// }
+
+// printName("Muaz")
+// printName("Basit")
+// printName("Moiz")
+
+
+// function sum(a,b) {
+//     console.log(a + b)
+// }
+// let number = 12323323132
+// let num2 = 3
+// sum(1,2)
+// sum(4343,34343)
+// sum(number,1000000000)
+
+// sum(number, num2)
+
+
+function additionOfNumbers(a,b,c,d) {
+    addition = a + b + c + d
+    return addition
 }
 
-printName("Muaz")
-printName("Basit")
-printName("Moiz")
+let Basit = 10.0
+let barcode2 = 10.0
 
-
-function sum(a,b) {
-    console.log(a + b)
-}
-let number = 12323323132
-let num2 = 3
-sum(1,2)
-sum(4343,34343)
-sum(number,1000000000)
-
-sum(number, num2)
+let sum = additionOfNumbers(Basit, barcode2, 3,4)
+console.log(sum)
