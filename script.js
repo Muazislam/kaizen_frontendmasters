89e2d76875d51f4dddaaaf3a17040216f5da2a5e
Muaz <muazislambabar@gmail.com>
2026-07-11 15:29:37 +0500

    Updating variables in Javascript study

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ let age = "25"
 console.log(age)
 
 
+            // Updating variable Values
+
+name = "Muaz"
+console.log(name)
+
+let score = 200
+console.log(score)
+
+score= 20
+console.log(score)
+
+score = 30
+console.log(score)
