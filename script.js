72a8e7c360d0e8ed8907a685c159289aee180c80
Muaz <muazislambabar@gmail.com>
2026-07-12 12:25:50 +0500

    function are vareiable

--- script.js
+++ script.js
@@ -1,41 +1,7 @@
-            // Creating function in js 
+        // functions as variables 
 
-// function sayHi() {
-//     console.log("Hi")
-// }
+function printName(name) {
+    console.log(name)
+}
 
-// sayHi()
-// sayHi()
-// sayHi()
-
-// function printName(name) {
-//     console.log(name)
-// }
-
-// printName("Muaz")
-// printName("Basit")
-// printName("Moiz")
-
-
-// function sum(a,b) {
-//     console.log(a + b)
-// }
-// let number = 12323323132
-// let num2 = 3
-// sum(1,2)
-// sum(4343,34343)
-// sum(number,1000000000)
-
-// sum(number, num2)
-
-
-// function additionOfNumbers(a,b,c,d) {
-//     addition = a + b + c + d
-//     return addition
-// }
-
-// let Basit = 10.0
-// let barcode2 = 10.0
-
-// let sum = additionOfNumbers(Basit, barcode2, 3,4)
-// console.log(sum)
+console.log(printName)
