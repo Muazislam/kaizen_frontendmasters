ad41348532e5a00d1f3f5d6e448553977e0dd966
Muaz <muazislambabar@gmail.com>
2026-07-12 12:33:10 +0500

    passing function to other variables

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
         // functions as variables 
 
-function printName(name) {
-    console.log(name)
-}
+// function printName(name) {
+//     console.log(name)
+// }
 
-console.log(printName)
+// console.log(printName)
+
+
+        // passing function to other variables 
+
+// function printName(name) {
+//     console.log(name)
+// }
+
+// function callFunction(x) {
+    
+//     x("Kyle")
+    
+// }
+
+// callFunction(printName)
+
