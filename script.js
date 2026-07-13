99315cf802f19fcf180fed6262dc43216ebc7342
Muaz <muazislambabar@gmail.com>
2026-07-14 02:52:59 +0500

    problems with var

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-var a = 1;
-console.log(a);
-var a = 2;
-console.log(a);
+// Problem with var
+
+// Var is function scope
+// which means that the function can access
+// the code from outside the curly braces in which it was declared
+// The inner code is accessible.
+// This is not possible because aaccessing data happens for
+// outside variales.
+
+const functionVar = () => {
+  {
+    var a = 1;
+  }
+  console.log(a);
+};
+
+let b = 2;
+functionVar();
+const functionLet = () => {
+  console.log(b);
+};
+functionLet();
