2df6d418c11386bb4ba6613ec2fc7a5b0c28a51c
Muaz <muazislambabar@gmail.com>
2026-07-13 07:00:36 +0500

    Accessing Multiple Scope Levels

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Closure 
+// Closure
 
 // const a = 1
 
@@ -7,7 +7,6 @@
 // }
 
 // print() // Prints: 1
-
 
 // Closures with Dynamic Values
 
@@ -20,5 +19,46 @@
 // a = 2
 // print()
 
-
 // Functions Inside Functions
+
+// function outer(a) {
+// //   function inner(b) {
+// //     console.log(a);
+// //     console.log(b);
+// //   }
+
+//   return inner;
+// }
+
+// const newFunc = outer(1)
+
+// // const newFunc = inner(b) {
+// //         console.log(a);
+// //         console.log(b);
+// //         }
+
+// newFunc(2) // inner(2)
+
+//=====================================
+//=====================================
+
+// Accessing Multiple Scope Levels
+
+// const c = 3; //Global scope
+
+// function outer(a) {
+//   function inner(b) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//   }
+
+//   return inner;
+// }
+
+// const newFunc = outer(1);
+// newFunc(2);
+
+//===================================
+//===================================
+
