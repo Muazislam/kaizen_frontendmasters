f18c74cd013a22329973b058126beb534fe51bb7
Muaz <muazislambabar@gmail.com>
2026-07-14 03:56:09 +0500

    Not Equals: != vs !==

--- script.js
+++ script.js
@@ -5,10 +5,19 @@
 
 // console.log(0 == false);
 // console.log(" " == false);
-console.log(1 == "2"); //Falso because after type coercion, js checks the values to be the same or not
+// console.log(1 == "2"); //Falso because after type coercion, js checks the values to be the same or not
 
-// Triple Equals === - No Type Coercion
-console.log(1 === 1);
-console.log(1 === "1");
-console.log(0 === "false");
-console.log("" === false);
+//================= Triple Equals === - No Type Coercion
+// console.log(1 === 1);
+// console.log(1 === "1");
+// console.log(0 === "false");
+// console.log("" === false);
+
+//==================  Not Equals: != vs !==
+
+//Here, 1 was not equal to "1". So.
+// the != pr !== gives opposite of what the real check value is
+// console.log(1 != "1")
+// console.log(1 !== "1")
+
+//==================  The Exception: null and undefined
