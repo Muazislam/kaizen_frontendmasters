4aaacbe0752b17231d740df1268a96b0a5464ffe
Muaz <muazislambabar@gmail.com>
2026-07-15 23:28:01 +0500

    Creating Objects, && Key-Value Pairs

--- script.js
+++ script.js
@@ -1,93 +1,12 @@
-// ===============  Arrays  ====================
+// seperate variables
+const name = "Kyle";
+const age = 30;
+const favoriteNumber = 3;
 
-// // single value\\
-// let numver = 5;
-// let nameOne = "Kyle";
+// Object (Organized together)
+const person = {
+  name: "Muaz", // key: name, value: "Muaz"
+  age: 30,  // key: age, value: 30
+  favoriteNumber: 3,    // key: favoriteNumber, Value: 3
+};
 
-// // Multiple values in an array
-// const number = [1, 2, 3, 4, 5];
-// const name = ["Kyle", "Sarah"];
-// const boolean = [true, false, true];
-
-// console.log(number);
-// console.log(name);
-// console.log(boolean);
-
-// =================================
-
-// creating array uses square brackets
-
-// const letters = ["a", "b", "s", `d`, `dsa`]
-
-// const longList = [ // format array on multiple lines for increase in readability
-//     `muaz`,
-//     `sheraz`,
-//     `Basit`,
-//     `Abdul Moiz`,
-// ]
-
-// console.log (longList)
-
-// const numbers = [1,2,3,4,5,6]
-
-// console.log(numbers[0])
-// console.log(numbers[1])
-// console.log(numbers[3])
-
-//==========================================
-
-// ====== Adding Elements to Arrays  =====
-// const data = [`mujtaba`, `hunan`, `mirzada`]
-
-// console.log(data)
-
-// data.push(`baist`)
-// console.log(data)
-
-// data.push(`sheraz`)
-// console.log(data)
-
-// ========================================
-
-// ========== Mixed Data Types =============
-
-// const mixedArray = [1, `hello`, 3.14, null, NaN]
-
-// console.log(mixedArray)
-// console.log(mixedArray[3])
-// console.log(mixedArray[4])
-// console.log(mixedArray[0])
-// console.log(mixedArray[1])
-
-// // ============  Array Length =============
-
-// console.log(mixedArray.length)
-// console.log(mixedArray.length)
-
-// const empty = []
-// console.log(empty.length)
-
-// =============== Nested Arrays =============
-
-const nestedArray = [
-  [`A`, `B`], //0
-  [`C`, `D`], //1
-];
-
-console.log(nestedArray[1]);
-console.log(nestedArray[0]);
-
-// ======  Accessing Nested Elements  ========
-
-const grid = [
-  [1, 2, 3, 0],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-
-console.log(grid[0]);
-console.log(grid[0][3]);
-console.log(grid[0][2]);
-console.log(grid[0][1]);
-
-console.log(grid[1][2]);
