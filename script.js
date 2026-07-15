901d8df62e73064aac0b36b2aecc854b4c0e3876
Muaz <muazislambabar@gmail.com>
2026-07-15 23:47:21 +0500

    Adding Functions to Objects

--- script.js
+++ script.js
@@ -3,12 +3,32 @@
 // const age = 30;
 // const favoriteNumber = 3;
 
-const person = {
-  name: "Muaz", // key: name, value: "Muaz"
-  age: 30, // key: age, value: 30
-  favoriteNumber: 3, // key: favoriteNumber, Value: 3
+//========== Accessing Object Properties ============
+
+// const person = {
+//   name: "Muaz", // key: name, value: "Muaz"
+//   age: 30, // key: age, value: 30
+//   favoriteNumber: 3, // key: favoriteNumber, Value: 3
+// };
+
+// console.log(person.age);
+// console.log(person.favoriteNumber)
+// console.log(person.name)
+
+// ===== Adding Functions to Objects =====
+
+const bioData = {
+  name: "muaz",
+  age: 21,
+  favourtieFood: `chocolate`,
+  education: `Bachelors`,
+
+  muazIs: function () {
+    console.log(`Hi`);
+  },
+  muaz: () => console.log(`hi`),
 };
 
-console.log(person.age);
-console.log(person.favoriteNumber)
-console.log(person.name)
+console.log(bioData.muaz); // wrong way to call function in object ""without putting paranthesis"""
+console.log(bioData.muazIs); // wrong way to call function in object ""without putting paranthesis"""
+console.log(bioData.muaz()); // correct way to call the funcition inside object
