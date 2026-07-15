d071d7bc0f65c573f38c046d6fc5fb6808bb3869
Muaz <muazislambabar@gmail.com>
2026-07-16 00:21:25 +0500

    Copying Reference Types

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // ======== Advanced Variables Reference vs Value ============
 
-const c = [1, 2];
-const d = c;
+// const c = [1, 2];
+// const d = c; // This copies the reference memory address.
+//             //  The memory address is not removed and pasted
+//             //  from that location.
 
-d.push(3);
-// Using references, we can modify an array or variable
-// without implementing changes directly on them
-// This is a convenient way to implement changes
-// to a huge array without needing to call each
-// index. but we can make changes
-// with just a seperate 1 variable to the whole game
-console.log(c);
+// d.push(3);
+// // Using references, we can modify an array or variable
+// // without implementing changes directly on them
+// // This is a convenient way to implement changes
+// // to a huge array without needing to call each
+// // index. but we can make changes
+// // with just a seperate 1 variable to the whole game
+// console.log(c);
+
