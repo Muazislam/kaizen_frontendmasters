b2711447f2c8242aec2cc345364044cf8d913dea
Muaz <muazislambabar@gmail.com>
2026-07-16 03:11:49 +0500

    Chaining Ternary Operators

--- script.js
+++ script.js
@@ -32,3 +32,17 @@ const message = age >= 18 ? `You can vote` : `You cannot vote`
 
 
 // --------> Chaining Ternary Operators
+
+const grad = score >= 90 ? `A` : score >= 80 ? `B` : score >= 70 ? `C` : `F`
+
+// If-else is clearer
+let grade
+if (score >= 90) {
+  grade = "A"
+} else if (score >= 80) {
+  grade = "B"
+} else if (score >= 70) {
+  grade = "C"
+} else {
+  grade = "F"
+}
