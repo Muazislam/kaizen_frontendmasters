2bbb806d72aaa9283b645714ce2d5e7bb31a2035
Muaz <muazislambabar@gmail.com>
2026-07-16 00:01:16 +0500

    Nested Objects

--- script.js
+++ script.js
@@ -35,6 +35,38 @@
 
 // ==== Shorthand Function Syntax ====
 
+// const bioData = {
+//   name: "muaz",
+//   age: 21,
+//   favourtieFood: `chocolate`,
+//   education: `Bachelors`,
+
+//   muazIs() {
+//     console.log(`Hi`);
+//   },
+//   muaz() {
+//     console.log(`hi`);
+//   },
+// };
+
+// console.log(bioData.muaz); // wrong way to call function in object ""without putting paranthesis"""
+// console.log(bioData.muazIs); // wrong way to call function in object ""without putting paranthesis"""
+// console.log(bioData.muaz()); // correct way to call the funcition inside object
+
+// // ==== Real-World Example: console.log ====
+
+// console.log("Hello"); //console is an object, log is the message
+
+// const console = {
+//   log(message) {
+//     // Display the message
+//   },
+// };
+
+//=============================================
+
+// =========== Nested Objects =======
+
 const bioData = {
   name: "muaz",
   age: 21,
@@ -47,18 +79,14 @@ const bioData = {
   muaz() {
     console.log(`hi`);
   },
-};
 
-console.log(bioData.muaz); // wrong way to call function in object ""without putting paranthesis"""
-console.log(bioData.muazIs); // wrong way to call function in object ""without putting paranthesis"""
-console.log(bioData.muaz()); // correct way to call the funcition inside object
-
-// ==== Real-World Example: console.log ====
-
-console.log("Hello"); //console is an object, log is the message
-
-const console = {
-  log(message) {
-    // Display the message
+  address: {
+    street: `12345 Main Street`,
+    city: `Karachi`
   },
+  hobbies: ["football", `copying github file`, `doing search for new tools`, `programming`]
 };
+
+console.log(bioData.address.city)
+console.log(bioData.address.street) // Accessing a sub property from an object property, inside the object.
+console.log(bioData.hobbies[0]) // Accessing array from the object property
