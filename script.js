9ee6e501638f975cc3299f9df64d2db37bc13f9f
Muaz <muazislambabar@gmail.com>
2026-07-15 23:35:16 +0500

    Accessing Object Properties

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // seperate variables
-const name = "Kyle";
-const age = 30;
-const favoriteNumber = 3;
+// const name = "Kyle";
+// const age = 30;
+// const favoriteNumber = 3;
 
-// Object (Organized together)
 const person = {
   name: "Muaz", // key: name, value: "Muaz"
-  age: 30,  // key: age, value: 30
-  favoriteNumber: 3,    // key: favoriteNumber, Value: 3
+  age: 30, // key: age, value: 30
+  favoriteNumber: 3, // key: favoriteNumber, Value: 3
 };
 
+console.log(person.age);
+console.log(person.favoriteNumber)
+console.log(person.name)
