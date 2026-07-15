7af88050e0632e35ff1fc855362708e0ebb872df
Muaz <muazislambabar@gmail.com>
2026-07-16 03:08:53 +0500

    Chaining Ternary Operators

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 // --------->  Ternary Operator
 
-// ========> Basic Syntax
+// ========>    Basic Syntax
+
+/*
 const age = 18
 age >= 18 ? `trueValue` : `falseValue`
+*/
 
 // ? is like an if
 // : is as else
 // if the condition before ? is true, it
 // returns the value after the ?
 // otherwise, it returns the value after the :
+
+//======== ============== ==============
+
+//======== ============== ==============
+
+//========>    Ternaries Return Values
+
+let message
+if(age >= 18){
+    message = `You can vote`
+} else {
+    message = `You cannot vote`
+}
+
+// Ternary operator implementation
+
+const message = age >= 18 ? `You can vote` : `You cannot vote`
+
+
+// --------> Chaining Ternary Operators
