02336d777100d13a7afe70306455b54318b84760
Muaz <muazislambabar@gmail.com>
2026-07-16 01:08:05 +0500

    this With Objects

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const m = `klsdf
-slkdjflksjdf
-alksdflkjds
-as;lkdjflkjdsafdsa
-lsaknsandvasi
-a;lksfpoa`;
+// ==== `This` keyword _ && _ classes ====
 
-console.log(m);
+// console.log(this) // In the browser, this by default sets the refers to the window object
+
+// ==== this With Objects ====
+const person = {
+  name: `Alice`,
+  age: 25,
+  greet() {
+    console.log(`Hello, my name is ${this.name}`);
+  },
+};
+        
+person.greet()
+// `this` is used inside the object to refer to the object itself
+// This is useful for accessing properties of the
+//  object from within its methods.
+
