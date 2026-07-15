4a433e378de56870b963290e540f7f4b4be579a8
Muaz <muazislambabar@gmail.com>
2026-07-16 00:36:00 +0500

    const with References

--- script.js
+++ script.js
@@ -39,3 +39,23 @@
 // // But creating a new object:
 // const person3 = { name: "Joe" }
 // console.log(person1 === person3) // false ← Different references
+
+// ========  ============  ===========  ==========
+
+// ====== const with References =====
+
+const arr = [1,2] // The arr (array) is being pointed to data
+                  // [1,2] and the const keyword creates
+                  // a strong condition that it won't change
+                  // it's value (reference) point (memory location)
+                  // ----
+                  // Therefore, we can modify the data(values) only at
+                  // that memory location. But modifying the memory 
+                  // location, by assigning-a-new-value(change in memory
+                  // location) is prohibited
+
+arr.push(3)
+console.log(arr) // [1,2,3]
+
+arr = [4,5]     // ❌ TypeError: Assignment to constant variable
+arr = [1,2]
