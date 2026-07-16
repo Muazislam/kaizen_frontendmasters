f682f7789ba6efb9db49d9ae08bb80326ecfbedd
Muaz <muazislambabar@gmail.com>
2026-07-17 00:02:22 +0500

    Iterating Through Arrays

--- script.js
+++ script.js
@@ -25,12 +25,24 @@
 
 // ==== Common Loop Patterns Skip Numbers ====
 
-for (let i = 0; i <= 10; i += 2) {  // i = i + 2
-                                    // i = 4 + 2 = 6
-  console.log(i);
-}
+// for (let i = 0; i <= 10; i += 2) {  // i = i + 2
+//                                     // i = 4 + 2 = 6
+//   console.log(i);
+// }
 
-// ==== Count down from 10 to 1 ====
-for (let i = 10; i >= 1; i--) {
-  console.log(i)
+// // ==== Count down from 10 to 1 ====
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i)
+// }
+
+// -----------------------------------
+// ===================================
+// -----------------------------------
+
+// ==== Iterating Through Arrays ====
+
+const fruits = [`apple`, `banana`, `orange`, `tangarange`];
+
+for (let i = 0; i <= 3; i++) {
+  console.log(`${i}: ${fruits[i]}`);
 }
