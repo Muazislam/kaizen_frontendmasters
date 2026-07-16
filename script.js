308b68a586e5065e8f62364ecbede4544151a496
Muaz <muazislambabar@gmail.com>
2026-07-16 23:54:13 +0500

    Simple Counting Example

--- script.js
+++ script.js
@@ -3,9 +3,18 @@
 // ====  Basic For Loop Syntax  ====
 
 // let i = 0     // Step1
-for (let i = 0; i < 5; i++) {  // Step 0
-  console.log(`Count ${i}`)   // Step 2
+for (let i = 0; i < 5; i++) {
+  // Step 0
+  console.log(`Count ${i}`); // Step 2
   // i++  // Step 3
 }
 
+// -----------------------------------
+// ===================================
+// -----------------------------------
 
+// ==== Simple Counting Example ====
+
+for (let n = 0; n < 60; n++) {
+  console.log(`Hi`);
+}
