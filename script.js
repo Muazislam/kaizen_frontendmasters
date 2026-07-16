45acd31ece9294bf0bc928ef8e9be457c95c68cc
Muaz <muazislambabar@gmail.com>
2026-07-17 00:22:29 +0500

    break - Exit the Loop Early

--- script.js
+++ script.js
@@ -64,3 +64,29 @@
 //     console.log(`${m} x ${p} = ${m * p}`)
 //   }
 // }
+
+// -----------------------------------
+// ===================================
+// -----------------------------------
+
+// ==== Loop Control Statements ====
+
+// -----> break - Exit the Loop Early
+
+const numbers = [1, 3, 7, 2, 9, 4];
+
+for (let i = 0; i < numbers.length; i++) { 
+  if (numbers[i] > 5) {
+    console.log(`Found: ${numbers[i]} at index ${i}`);
+    break;
+  }
+}
+// The number.length purpose is to tell the for loop to 
+// make the loop iteration only to the size of the array.
+// Working:
+//     numbers = [1, 3, 7, 2, 9, 4] -----> has size of 6.
+//    But the index number of last digit four is 5.
+//    Therefore, we use numbers.length and make the 
+//    loop condition less than to the value of length.
+//    So, the loop will iterate automatically only to the
+//    last index number of the loop.
