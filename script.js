0833dbb4e35044fe016bfa3d5a6261350c003172
Muaz <muazislambabar@gmail.com>
2026-07-17 06:32:08 +0500

    When to Use Recursion

--- script.js
+++ script.js
@@ -26,4 +26,39 @@
 
 // countdown(3)
 
+// <<----------When to Use Recursion--------->>
 
+const fileSystem = {
+  name: `root`,
+  type: `folder`,
+  children: [
+    {
+      name: `documents`,
+      type: `folder`,
+      children: [
+        { name: `resume.pdf`, type: `file` },
+        { name: `notes.txt`, type: `file` },
+      ],
+    },
+    {
+      name: 'photos',
+      type: `folder`,
+      children: [{name: `vacation.jpg`, type: `file`}],
+
+    }
+    {name: readme.txt, type: `file`}
+  ],
+};
+
+function countFiles(item) {
+  if(item.type === `file`) return 1
+
+  let count = 0
+
+  for (const child of item.children){
+    count += countFiles(child)
+  }
+  return count
+}
+
+console.log(countFiles(filesystem))
