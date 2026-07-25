3abe5a935da8f04983af7cd9472055da47c8c0d5
Muaz <muazislambabar@gmail.com>
2026-07-25 15:02:39 +0500

    private in OOP js

--- script.js
+++ script.js
@@ -18,18 +18,26 @@ p.greet(); // Hi, I'm Alex
 // And only the defined methods can access it
 
 class Counter {
-  #count = 0;       // private -- can't be accessed outside this class
+  #count = 0; // private -- can't be accessed outside this class
 
   increment() {
-    this.#count++;
+    // this is a method and a void funcition that we produce that is used to increment in
+    // the private property.
+    this.#count++; // this here is acting like a pointer and it refers to the private property (object reference being created)
   }
 
   getCount() {
     return this.#count;
+    // this is a funciton or method that specifically returns the value to the 
   }
 }
 
 const c = new Counter();
-c.increment();
+c.increment(); //  here c is like at the place of constructoer and it works like a function when we call a function, i think we write the call function same like this now when we are calling the class.
 console.log(c.getCount());
 console.log(c.#count);
+
+// the bottom prints the result of the private method so, it is not printed.
+// is the private a class within a class?
+
+
